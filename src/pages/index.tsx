01f16e936b2352239ed2e71de667137c25cc9ace
alwjrqns <yyb02006@gmail.com>
2023-04-17 01:33:39 +0900

    레이아웃 컴포넌트 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
+import Layout from '@/components/layout';
+
 export default function Home() {
 	return (
-		<div>
-			<ul className='text-3xl font-bold'>
-				<li className='font-SCoreDream'>에스코어드림</li>
-				<li className='font-Pretendard'>에스코어드림</li>
-				<li className='font-GmarketSans'>에스코어드림</li>
-				<li className='font-Roboto'>에스코어드림</li>
-			</ul>
-		</div>
+		<Layout seoTitle='INSAN'>
+			<div className='bg-[#101010] min-h-screen'></div>
+		</Layout>
 	);
 }
