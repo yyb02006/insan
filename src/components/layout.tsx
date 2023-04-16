4cfd8de0d73021e112c634b42fe142e0470e61dd
alwjrqns <yyb02006@gmail.com>
2023-04-17 01:33:39 +0900

    레이아웃 컴포넌트 추가

---
+++ src/components/layout.tsx
@@ -0,0 +1,42 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { ReactNode } from 'react';
+
+interface LayoutProps {
+	seoTitle: string;
+	children: ReactNode;
+}
+
+export default function Layout({ seoTitle, children }: LayoutProps) {
+	const router = useRouter();
+	return (
+		<div>
+			<Head>
+				<title>
+					{router.pathname === '/' ? `${seoTitle}` : `${seoTitle} | INSAN`}
+				</title>
+			</Head>
+			<nav className='fixed bg-[#101010] w-full h-[100px]'>
+				<div className='mx-[60px] h-full flex justify-between items-center'>
+					<Link href={'/'}>
+						<Image
+							src='/images/Logo.svg'
+							alt='INSAN'
+							width={28}
+							height={42}
+							className='cursor-pointer'
+						/>
+					</Link>
+					<ul className='font-Roboto font-light text-[15px] text-[#E1E1E1] flex gap-9'>
+						<Link href={'/work'}>Work</Link>
+						<Link href={'/about'}>About</Link>
+						<Link href={'/contact'}>Contact us</Link>
+					</ul>
+				</div>
+			</nav>
+			{children}
+		</div>
+	);
+}
