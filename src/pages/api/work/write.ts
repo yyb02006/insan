c9f449039091f4c9f2d27a42e8c1608bcde5ec0e
alwjrqns <yyb02006@gmail.com>
2023-05-30 01:54:45 +0900

    write api 구현, 프리즈마 모델 수정

--- src/pages/api/work/write.ts
+++ src/pages/api/work/write.ts
@@ -1,10 +1,27 @@
 import client from '@/libs/server/client';
 import withHandler from '@/libs/server/withHandler';
+import { WorkInfos } from '@/pages/work/write';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
 	const { body } = req;
-	res.status(202).end();
+	console.log(body);
+	body.forEach(async (el: WorkInfos) => {
+		await client.works.upsert({
+			where: { resourceId: el.resourceId },
+			create: {
+				title: el.title,
+				resourceId: el.resourceId,
+				description: el.description,
+			},
+			update: {
+				title: el.title,
+				resourceId: el.resourceId,
+				description: el.description,
+			},
+		});
+	});
+	res.json({ success: true });
 }
 
 export default withHandler({ methods: ['POST'], handlerFunc: handler });
