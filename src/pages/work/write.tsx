14c5809d493af35aea6bde5acbde0ccc3eb26c10
alwjrqns <yyb02006@gmail.com>
2023-06-10 03:22:03 +0900

    카테고리 버튼 추가

--- src/pages/work/write.tsx
+++ src/pages/work/write.tsx
@@ -15,6 +15,9 @@ export interface WorkInfos {
 }
 
 export default function Write() {
+	const [category, setCategory] = useState<'film&short' | 'outsource'>(
+		'film&short'
+	);
 	const lists = [
 		{ title: '외주 작업', id: 'PL3Sx9O__-BGnKsABX4khAMW6BBFF_Hf40' },
 		{ title: '참여 촬영', id: 'PL3Sx9O__-BGlyWzd0DnpZT9suTNy4kBW1' },
@@ -123,156 +126,188 @@ export default function Write() {
 						<Link href={'/work/delete'}>삭제하기</Link>
 					</div>
 				</div>
-				<div>카테고리</div>
-				<form
-					onSubmit={() => {}}
-					className='relative mb-8 font-light flex items-center gap-2 pb-1 border-b border-[#9a9a9a] text-lg leading-tight text-[#eaeaea]'
-				>
-					<button>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							fill='none'
-							viewBox='0 0 24 24'
-							strokeWidth={2}
-							stroke='currentColor'
-							className='w-6 h-6'
-						>
-							<path
-								strokeLinecap='round'
-								strokeLinejoin='round'
-								d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
-							/>
-						</svg>
+				<div className='flex py-4'>
+					<button
+						onClick={() => {
+							setCategory('film&short');
+						}}
+						className={cls(
+							category === 'film&short' ? 'text-palettered' : '',
+							'w-full flex justify-center items-center text-lg font-semibold hover:text-palettered'
+						)}
+					>
+						Film / Short
 					</button>
-					<Input
-						name='search'
-						type='text'
-						placeholder='search'
-						css='border-none placeholder:font-bold bg-transparent'
-						onChange={() => {}}
-					/>
-				</form>
-				<div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-6 gap-y-12 '>
-					{list.map((data, arr) => (
-						<div key={arr} className='w-full flex flex-col justify-between'>
-							<div>
-								<Image
-									src={
-										list.length !== 0
-											? data.snippet.thumbnails.maxres?.url ||
-											  data.snippet.thumbnails.medium?.url
-											: ''
-									}
-									alt='test'
-									width={1280}
-									height={720}
-									className='w-full object-cover'
-								/>
-								<div className='mt-2'>
-									<div className='text-sm'>Title : {data.snippet.title}</div>
-									<div className='text-xs font-light'>
-										Id : {data.snippet.resourceId?.videoId}
+					<button
+						onClick={() => {
+							setCategory('outsource');
+						}}
+						className={cls(
+							category === 'outsource' ? 'text-palettered' : '',
+							'w-full flex justify-center items-center text-lg font-semibold hover:text-palettered'
+						)}
+					>
+						Outsource
+					</button>
+				</div>
+				{category === 'film&short' ? <></> : null}
+				{category === 'outsource' ? (
+					<>
+						<form
+							onSubmit={() => {}}
+							className='relative mb-8 mt-4 font-light flex items-center gap-2 pb-1 border-b border-[#9a9a9a] text-lg leading-tight text-[#eaeaea]'
+						>
+							<button>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									strokeWidth={2}
+									stroke='currentColor'
+									className='w-6 h-6'
+								>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
+									/>
+								</svg>
+							</button>
+							<Input
+								name='search'
+								type='text'
+								placeholder='search'
+								css='border-none placeholder:font-bold bg-transparent'
+								onChange={() => {}}
+							/>
+						</form>
+						<div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-6 gap-y-12 '>
+							{list.map((data, arr) => (
+								<div key={arr} className='w-full flex flex-col justify-between'>
+									<div>
+										<Image
+											src={
+												list.length !== 0
+													? data.snippet.thumbnails.maxres?.url ||
+													  data.snippet.thumbnails.medium?.url
+													: ''
+											}
+											alt='test'
+											width={1280}
+											height={720}
+											className='w-full object-cover'
+										/>
+										<div className='mt-2'>
+											<div className='text-sm'>
+												Title : {data.snippet.title}
+											</div>
+											<div className='text-xs font-light'>
+												Id : {data.snippet.resourceId?.videoId}
+											</div>
+										</div>
+									</div>
+									<div className='mt-2'>
+										<Input
+											name='title'
+											type='text'
+											placeholder='타이틀'
+											data-resourceid={
+												data.snippet.resourceId
+													? data.snippet.resourceId?.videoId
+													: ''
+											}
+											onChange={InputChange}
+											value={
+												workInfos?.find((arr) => {
+													return (
+														arr.resourceId === data.snippet.resourceId?.videoId
+													);
+												})?.title
+													? workInfos.find((arr) => {
+															return (
+																arr.resourceId ===
+																data.snippet.resourceId?.videoId
+															);
+													  })?.title
+													: ''
+											}
+										/>
+										<Input
+											name='description'
+											type='text'
+											placeholder='설명'
+											data-resourceid={
+												data.snippet.resourceId
+													? data.snippet.resourceId?.videoId
+													: ''
+											}
+											onChange={InputChange}
+											value={
+												workInfos?.find((arr) => {
+													return (
+														arr.resourceId === data.snippet.resourceId?.videoId
+													);
+												})?.description
+													? workInfos.find((arr) => {
+															return (
+																arr.resourceId ===
+																data.snippet.resourceId?.videoId
+															);
+													  })?.description
+													: ''
+											}
+										/>
+										<Input
+											type='radio'
+											name='category'
+											value='film'
+											radioId={`${data.id}film`}
+											onClick={InputChange}
+											data-resourceid={
+												data.snippet.resourceId
+													? data.snippet.resourceId?.videoId
+													: ''
+											}
+											css=''
+											labelCss=''
+											labelName='film'
+										/>
+										<Input
+											type='radio'
+											name='category'
+											value='short'
+											radioId={`${data.id}short`}
+											onClick={InputChange}
+											data-resourceid={
+												data.snippet.resourceId
+													? data.snippet.resourceId?.videoId
+													: ''
+											}
+											css=''
+											labelCss=''
+											labelName='short'
+										/>
+										<Input
+											type='radio'
+											name='category'
+											value='outsource'
+											radioId={`${data.id}outsource`}
+											onClick={InputChange}
+											data-resourceid={
+												data.snippet.resourceId
+													? data.snippet.resourceId?.videoId
+													: ''
+											}
+											css=''
+											labelCss=''
+											labelName='outsource'
+										/>
 									</div>
 								</div>
-							</div>
-							<div className='mt-2'>
-								<Input
-									name='title'
-									type='text'
-									placeholder='타이틀'
-									data-resourceid={
-										data.snippet.resourceId
-											? data.snippet.resourceId?.videoId
-											: ''
-									}
-									onChange={InputChange}
-									value={
-										workInfos?.find((arr) => {
-											return (
-												arr.resourceId === data.snippet.resourceId?.videoId
-											);
-										})?.title
-											? workInfos.find((arr) => {
-													return (
-														arr.resourceId === data.snippet.resourceId?.videoId
-													);
-											  })?.title
-											: ''
-									}
-								/>
-								<Input
-									name='description'
-									type='text'
-									placeholder='설명'
-									data-resourceid={
-										data.snippet.resourceId
-											? data.snippet.resourceId?.videoId
-											: ''
-									}
-									onChange={InputChange}
-									value={
-										workInfos?.find((arr) => {
-											return (
-												arr.resourceId === data.snippet.resourceId?.videoId
-											);
-										})?.description
-											? workInfos.find((arr) => {
-													return (
-														arr.resourceId === data.snippet.resourceId?.videoId
-													);
-											  })?.description
-											: ''
-									}
-								/>
-								<Input
-									type='radio'
-									name='category'
-									value='film'
-									radioId={`${data.id}film`}
-									onClick={InputChange}
-									data-resourceid={
-										data.snippet.resourceId
-											? data.snippet.resourceId?.videoId
-											: ''
-									}
-									css=''
-									labelCss=''
-									labelName='film'
-								/>
-								<Input
-									type='radio'
-									name='category'
-									value='short'
-									radioId={`${data.id}short`}
-									onClick={InputChange}
-									data-resourceid={
-										data.snippet.resourceId
-											? data.snippet.resourceId?.videoId
-											: ''
-									}
-									css=''
-									labelCss=''
-									labelName='short'
-								/>
-								<Input
-									type='radio'
-									name='category'
-									value='outsource'
-									radioId={`${data.id}outsource`}
-									onClick={InputChange}
-									data-resourceid={
-										data.snippet.resourceId
-											? data.snippet.resourceId?.videoId
-											: ''
-									}
-									css=''
-									labelCss=''
-									labelName='outsource'
-								/>
-							</div>
+							))}
 						</div>
-					))}
-				</div>
+					</>
+				) : null}
 				<div className='sm:w-[60px] flex sm:block h-14 sm:h-auto w-full sm:ring-1 sm:ring-palettered sm:rounded-full fixed xl:right-20 sm:right-4 right-0 sm:top-[100px] sm:bottom-auto bottom-0'>
 					<button
 						onClick={onReset}
