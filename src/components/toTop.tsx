c2df5634ee47ef170e1272cb382baac95c8fda74
alwjrqns <yyb02006@gmail.com>
2023-06-28 23:47:58 +0900

    toTop버튼 수정

--- src/components/toTop.tsx
+++ src/components/toTop.tsx
@@ -14,9 +14,22 @@ export default function ToTop({ toScroll }: ToTopProps) {
 	return (
 		<button
 			onClick={onMoveClick}
-			className='fixed rounded-full bg-green-500 right-6 bottom-6 w-16 aspect-square'
+			className='fixed rounded-full bg-green-500 right-6 bottom-6 w-12 aspect-square flex justify-center items-center'
 		>
-			toTop
+			<svg
+				xmlns='http://www.w3.org/2000/svg'
+				fill='none'
+				viewBox='0 0 24 24'
+				strokeWidth={2}
+				stroke='currentColor'
+				className='w-full h-full p-3'
+			>
+				<path
+					strokeLinecap='round'
+					strokeLinejoin='round'
+					d='M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18'
+				/>
+			</svg>
 		</button>
 	);
 }
