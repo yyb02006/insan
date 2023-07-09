efc4d06bc9bf4b1c79bbf496213099088a38b618
alwjrqns <yyb02006@gmail.com>
2023-07-09 23:54:56 +0900

    feed 컴포넌트 수정

--- src/components/feed.tsx
+++ src/components/feed.tsx
@@ -8,81 +8,116 @@ interface feedProps {
 	searchResult: GapiItem[];
 	inputChange: (e: SyntheticEvent<HTMLInputElement>) => void;
 	workInfos: WorkInfos[] | undefined;
+	host: 'youtube' | 'vimeo';
 }
 
-export default function Feed({
+export default function ThumnailFeed({
 	searchResult,
 	inputChange,
 	workInfos,
+	host,
 }: feedProps) {
 	return (
-		<div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-6 gap-y-12 '>
-			{searchResult.map((data, arr) => (
-				<div key={arr} className='w-full flex flex-col justify-between'>
-					<div>
-						<Image
-							src={
-								searchResult.length !== 0
-									? data.snippet.thumbnails.maxres?.url ||
-									  data.snippet.thumbnails.medium?.url
-									: ''
-							}
-							alt='test'
-							width={1280}
-							height={720}
-							className='w-full object-cover'
-						/>
-						<div className='mt-2'>
-							<div className='text-sm'>Title : {data.snippet.title}</div>
-							<div className='text-xs font-light'>
-								Id : {data.snippet.resourceId?.videoId}
+		<>
+			{host === 'youtube' ? (
+				<div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-6 gap-y-12 '>
+					{searchResult.map((data, arr) => (
+						<div key={arr} className='w-full flex flex-col justify-between'>
+							<div>
+								<Image
+									src={
+										searchResult.length !== 0
+											? data.snippet.thumbnails.maxres?.url ||
+											  data.snippet.thumbnails.medium?.url
+											: ''
+									}
+									alt='test'
+									width={1280}
+									height={720}
+									className='w-full object-cover'
+								/>
+								<div className='mt-2'>
+									<div className='text-sm'>Title : {data.snippet.title}</div>
+									<div className='text-xs font-light'>
+										Id : {data.snippet.resourceId?.videoId}
+									</div>
+								</div>
+							</div>
+							<div className='mt-2'>
+								<Input
+									name='title'
+									type='text'
+									placeholder='타이틀'
+									data-resourceid={
+										data.snippet.resourceId
+											? data.snippet.resourceId?.videoId
+											: ''
+									}
+									onChange={inputChange}
+									value={
+										workInfos?.find((arr) => {
+											return (
+												arr.resourceId === data.snippet.resourceId?.videoId
+											);
+										})?.title
+											? workInfos.find((arr) => {
+													return (
+														arr.resourceId === data.snippet.resourceId?.videoId
+													);
+											  })?.title
+											: ''
+									}
+								/>
+								<Input
+									name='description'
+									type='text'
+									placeholder='설명'
+									data-resourceid={
+										data.snippet.resourceId
+											? data.snippet.resourceId?.videoId
+											: ''
+									}
+									onChange={inputChange}
+									value={
+										workInfos?.find((arr) => {
+											return (
+												arr.resourceId === data.snippet.resourceId?.videoId
+											);
+										})?.description
+											? workInfos.find((arr) => {
+													return (
+														arr.resourceId === data.snippet.resourceId?.videoId
+													);
+											  })?.description
+											: ''
+									}
+								/>
 							</div>
 						</div>
-					</div>
-					<div className='mt-2'>
-						<Input
-							name='title'
-							type='text'
-							placeholder='타이틀'
-							data-resourceid={
-								data.snippet.resourceId ? data.snippet.resourceId?.videoId : ''
-							}
-							onChange={inputChange}
-							value={
-								workInfos?.find((arr) => {
-									return arr.resourceId === data.snippet.resourceId?.videoId;
-								})?.title
-									? workInfos.find((arr) => {
-											return (
-												arr.resourceId === data.snippet.resourceId?.videoId
-											);
-									  })?.title
-									: ''
-							}
-						/>
-						<Input
-							name='description'
-							type='text'
-							placeholder='설명'
-							data-resourceid={
-								data.snippet.resourceId ? data.snippet.resourceId?.videoId : ''
-							}
-							onChange={inputChange}
-							value={
-								workInfos?.find((arr) => {
-									return arr.resourceId === data.snippet.resourceId?.videoId;
-								})?.description
-									? workInfos.find((arr) => {
-											return (
-												arr.resourceId === data.snippet.resourceId?.videoId
-											);
-									  })?.description
-									: ''
-							}
-						/>
-					</div>
+					))}
 				</div>
-			))}
-		</div>
+			) : null}
+			{host === 'vimeo' ? (
+				<>
+					<div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-6 gap-y-12 '>
+						{vimeoVideos.map((video) => (
+							<div
+								key={video.resource_key}
+								className='relative w-auto aspect-video bg-green-600'
+							>
+								<Image
+									src={video.pictures.sizes[4].link}
+									alt='picturesAlter'
+									width={960}
+									height={540}
+									priority
+									className='absolute top-0 left-0'
+								></Image>
+							</div>
+						))}
+					</div>
+				</>
+			) : null}
+		</>
 	);
 }
