011279af5dc276c07f276450a23d752968f68e09
alwjrqns <yyb02006@gmail.com>
2023-07-15 01:33:32 +0900

    work페이지 film, short api 수정

--- src/pages/api/work/list.ts
+++ src/pages/api/work/list.ts
@@ -3,13 +3,13 @@ import withHandler from '@/libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-	const {
-		query: { kind },
-	} = req;
-	if (kind === 'outsource') {
-		const work = await client.works.findMany();
-		res.json({ success: true, work });
-	}
+	const list = await client.works.findMany();
+	const work = {
+		film: list.filter((el) => el.category === 'film'),
+		short: list.filter((el) => el.category === 'short'),
+		outsource: list.filter((el) => el.category === 'outsource'),
+	};
+	res.json({ success: true, work });
 };
 
 export default withHandler({ methods: ['GET'], handlerFunc: handler });
