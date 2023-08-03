78bf606af9ad96b9d65653c1e0860ed86a6dd428
alwjrqns <yyb02006@gmail.com>
2023-08-04 05:52:01 +0900

    videoDetail,fetchYouTubeApi 수정, 코드 합병

--- src/libs/client/utils.ts
+++ src/libs/client/utils.ts
@@ -22,15 +22,18 @@ export function fetchApi<T>(
  * YouTubeFetch를 처리해주는 함수
  */
 export function fetchYouTubeApi<T>(
-	method: 'playlists' | 'playlistItems',
+	method: 'playlists' | 'playlistItems' | 'videos',
 	maxResult: string,
 	setFunc: (value: T) => void,
 	fields?: string,
-	playlistId?: string
+	playlistId?: string,
+	videosId?: string
 ) {
 	const apiKey = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
 	fetchApi(
-		`https://www.googleapis.com/youtube/v3/${method}?key=${apiKey}&part=snippet&maxResults=${maxResult}${
+		`https://www.googleapis.com/youtube/v3/${method}?${
+			method === 'videos' ? `id=${videosId}&` : ''
+		}key=${apiKey}&part=snippet&maxResults=${maxResult}${
 			method === 'playlists' ? '&channelId=UCwy8JhA4eDumalKwKrvrxQA' : ''
 		}${method === 'playlistItems' ? `&playlistId=${playlistId}` : ''}${
 			fields ? `&fields=${fields}` : ''
