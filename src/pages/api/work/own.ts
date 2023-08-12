2ca35ff581767335a05426a2ee5171da835d54b2
alwjrqns <yyb02006@gmail.com>
2023-08-12 19:19:27 +0900

    useUser훅 작성, api엔드포인트 수정

--- src/pages/api/work/own.ts
+++ src/pages/api/work/own.ts
@@ -4,10 +4,13 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	const { session } = req;
-	console.log(session.admin);
-	res.status(200).json({ success: true });
+	if (session?.admin) {
+		res.status(200).json({ success: true, message: 'Autorized' });
+	} else {
+		res.status(200).json({ success: false, message: 'Unautorized' });
+	}
 };
 
 export default apiSessionWrapper(
-	withHandler({ methods: ['POST'], handlerFunc: handler })
+	withHandler({ methods: ['GET'], handlerFunc: handler })
 );
