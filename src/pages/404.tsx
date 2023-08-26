dc6659c754a2796b4aecf2bb131bae1bbfa68f21
alwjrqns <yyb02006@gmail.com>
2023-08-27 04:24:39 +0900

    404에러페이지 커스텀

---
+++ src/pages/404.tsx
@@ -0,0 +1,30 @@
+import { useRouter } from 'next/router';
+
+export default function Error403() {
+	const router = useRouter();
+	const onButtonClick = () => {
+		router.push('/');
+	};
+	return (
+		<div className='w-screen h-screen flex flex-col justify-center items-center'>
+			<div className='text-sm font-medium'>
+				<span className='text-palettered'>404</span> 에러발생!{' '}
+				<span className='text-[#606060]'>
+					{'( 404TMI : 보여줄 페이지가 없다는 뜻 )'}
+				</span>
+			</div>
+			<div className='mt-6 mb-16 underline underline-offset-8 text-6xl leading-tight text-[#eaeaea] font-bold text-center'>
+				정말 <span className='text-palettered'>{'<죄송!/>'}</span>합니다.
+				<br />이 페이지는 <span className='text-palettered'>없는</span> 페이지
+				입니다.
+			</div>
+			<button
+				onClick={onButtonClick}
+				className='border p-4 rounded-2xl hover:border-palettered hover:text-palettered'
+			>
+				메인페이지로 <span className='text-palettered'>{`<냉큼/>`}</span>
+				돌아가기
+			</button>
+		</div>
+	);
+}
