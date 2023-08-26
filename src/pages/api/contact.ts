c5c15385520953faf73e3def47a5d120936c1fdc
alwjrqns <yyb02006@gmail.com>
2023-08-26 17:07:05 +0900

    contact 엔드포인트 변경

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -17,27 +17,35 @@ const transporter = nodemailer.createTransport({
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
 	const { title, email, message } = req.body;
-	let result;
-	try {
-		await transporter.sendMail(
-			{
-				from: process.env.NAVER_USER,
-				to: process.env.NAVER_TEST,
-				subject: `${title}`,
-				html: `<div style='background-Color:#101010; padding:32px; color:#eaeaea;'><div style='font-weight:800; font-size:2.5rem; margin-bottom:24px;'>${title}</div><div style='font-weight:400; font-size:1rem'><div>email : ${email}</div><div>message : ${message}</div></div></div>`,
-			},
-			(err, info) => {
-				if (err) {
-					result = err;
-				} else {
-					result = 'success';
-				}
+	await new Promise((resolve, reject) => {
+		transporter.verify((error, success) => {
+			if (error) {
+				console.log(error);
+				reject(error);
+			} else {
+				console.log('Server is ready to take our messages');
+				resolve(success);
 			}
-		);
-		res.status(200).end();
-	} catch (error) {
-		res.status(500).json({ success: false, message: result });
-	}
+		});
+	});
+	const mailData = {
+		from: process.env.NAVER_USER,
+		to: process.env.NAVER_TEST,
+		subject: `${title}`,
+		html: `<div style='background-Color:#101010; padding:32px; color:#eaeaea;'><div style='font-weight:800; font-size:2.5rem; margin-bottom:24px;'>${title}</div><div style='font-weight:400; font-size:1rem'><div>email : ${email}</div><div>message : ${message}</div></div></div>`,
+	};
+	await new Promise((resolve, reject) => {
+		transporter.sendMail(mailData, (error, info) => {
+			if (error) {
+				console.log(error);
+				reject(error);
+			} else {
+				console.log(info);
+				resolve(info);
+			}
+		});
+	});
+	res.status(200).json({ success: true });
 }
 
 export default withHandler({ methods: ['POST'], handlerFunc: handler });
