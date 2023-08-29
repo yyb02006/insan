00e804fb7d2296409cee34670e190945e2bff04c
alwjrqns <yyb02006@gmail.com>
2023-08-29 22:16:42 +0900

    animation 제거 lcp 테스트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -301,15 +301,15 @@ const SnsLink: NextPage<SnsLinkProps> = ({ scrollYProgress, isInView }) => {
 			className='fixed right-0 w-full h-full flex justify-end items-end text-[#efefef] z-[1]'
 		>
 			<m.div
-				style={{ scale }}
+				/* style={{ scale }}
 				initial='hidden'
 				animate='visible'
-				variants={sideCircle}
+				variants={sideCircle} */
 				className='absolute w-[200px] lg:w-[260px] aspect-square rounded-full border border-[#bababa] -bottom-12 -right-10 origin-bottom-right'
 			/>
 			<m.ul
-				animate={!isInView ? 'visible' : 'disappear'}
-				variants={snsAnchor}
+				/* animate={!isInView ? 'visible' : 'disappear'}
+				variants={snsAnchor} */
 				className='pr-[40px] md:pr-[60px] pb-8 flex flex-col items-end font-Roboto font-light text-sm lg:text-lg gap-2 z-[1]'
 			>
 				{[
@@ -1198,29 +1198,30 @@ const Home: NextPage = () => {
 		>
 			<Chevron scrollYProgress={scrollYProgress} isInView={isInView} />
 			<SnsLink scrollYProgress={scrollYProgress} isInView={isInView} />
-			{/* <Layout
+			<Layout
 				seoTitle='INSAN'
 				nav={{
 					isShort: innerWidth > 640 ? !isInBackground : true,
 				}}
-			> */}
-			<CircleSection
-				inheritRef={circle}
-				innerWidth={innerWidth}
-				mouseX={mouseX}
-				mouseY={mouseY}
-				scrollYProgress={scrollYProgress}
-				isMobile={isMobile}
-			/>
-			<WavesSection
-				inheritRef={wave}
-				scrollYProgress={scrollYProgress}
-				innerWidth={innerWidth}
-			/>
-			<VideosSection isMobile={isMobile} innerWidth={innerWidth} />
-			<TextSection />
-			<OutroSection />
-			{/* </Layout> */}
+				menu={false}
+			>
+				<CircleSection
+					inheritRef={circle}
+					innerWidth={innerWidth}
+					mouseX={mouseX}
+					mouseY={mouseY}
+					scrollYProgress={scrollYProgress}
+					isMobile={isMobile}
+				/>
+				<WavesSection
+					inheritRef={wave}
+					scrollYProgress={scrollYProgress}
+					innerWidth={innerWidth}
+				/>
+				<VideosSection isMobile={isMobile} innerWidth={innerWidth} />
+				<TextSection />
+				<OutroSection />
+			</Layout>
 		</div>
 	);
 };
