a1e1f43f207677d5e06b695a8854b4fa74728887
alwjrqns <yyb02006@gmail.com>
2023-08-29 23:44:44 +0900

    에러페이지 반응형 적용

--- src/pages/403.tsx
+++ src/pages/403.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link';
 export default function Error403() {
 	return (
 		<div className='w-screen h-screen flex flex-col justify-center items-center gap-5'>
-			<div className='text-sm font-medium'>
+			<div className='lg:text-sm text-xs font-medium'>
 				<span className='text-palettered'>403</span> 에러발생!{' '}
 				<span className='text-palettered'>403</span> 에러발생!
 			</div>
-			<div className='mb-8 text-6xl text-palettered leading-tight text-center font-bold'>
+			<div className='mb-8 lg:leading-snug sm:leading-normal leading-relaxed lg:text-[3.4vw] sm:text-[2rem] text-[1.875rem] text-palettered text-center font-bold'>
 				워<span className='text-[#fafafa]'>닝</span>! 워
 				<span className='text-[#fafafa]'>닝</span>!<p>당신... 봇 아니야?!</p>
 			</div>
@@ -15,7 +15,8 @@ export default function Error403() {
 				href={'/'}
 				className='border p-4 rounded-2xl hover:border-palettered hover:text-palettered'
 			>
-				메인페이지로 {`<냉큼/>`}돌아가기
+				메인페이지로 <span className='text-palettered'>{`<냉큼/>`}</span>
+				돌아가기
 			</Link>
 		</div>
 	);
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link';
 export default function Error403() {
 	return (
 		<div className='w-screen h-screen flex flex-col justify-center items-center'>
-			<div className='text-sm font-medium'>
+			<div className='lg:text-sm text-xs font-medium'>
 				<span className='text-palettered'>404</span> 에러발생!{' '}
 				<span className='text-[#606060]'>
 					{'( 404TMI : 보여줄 페이지가 없다는 뜻 )'}
 				</span>
 			</div>
-			<div className='mt-6 mb-16 underline underline-offset-8 text-6xl leading-tight text-[#eaeaea] font-bold text-center'>
+			<div className='mt-6 mb-16 underline underline-offset-8 lg:leading-snug sm:leading-normal leading-relaxed lg:text-[3.4vw] sm:text-[2rem] text-[1.875rem] text-[#eaeaea] font-bold text-center'>
 				정말 <span className='text-palettered'>{'<죄송!/>'}</span>합니다.
 				<br />이 페이지는 <span className='text-palettered'>없는</span> 페이지
 				입니다.
