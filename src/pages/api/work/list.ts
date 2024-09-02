6d94a9e3794484b53a7e6ec6de1aed42acaa09c8
alwjrqns <yyb02006@gmail.com>
2024-09-02 23:47:20 +0900

    Fix : /list api라우트 요청시 데이터의 정렬 방법 변경
    - id 기준의 역순

--- src/pages/api/work/list.ts
+++ src/pages/api/work/list.ts
@@ -4,72 +4,71 @@ import { Works } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export interface VideoResponseItem {
-	film: Works[];
-	short: Works[];
-	outsource: Works[];
+  film: Works[];
+  short: Works[];
+  outsource: Works[];
 }
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-	const { query } = req;
-	const { page, per_page, category } = query;
-	if (!page || !per_page) return;
-	const take = +per_page;
-	const skip = +per_page * (+page - 1);
-	let works: VideoResponseItem = {
-		film: [],
-		short: [],
-		outsource: [],
-	};
-	if (category === 'film') {
-		works.film = await client.works.findMany({
-			where: { category: 'film' },
-			skip,
-			take,
-		});
-	} else if (category === 'short') {
-		works.short = await client.works.findMany({
-			where: { category: 'short' },
-			skip,
-			take,
-		});
-	} else if (category === 'filmShort') {
-		works.film = await client.works.findMany({
-			where: { OR: [{ category: 'film' }, { category: 'short' }] },
-			skip,
-			take,
-		});
-	} else if (category === 'outsource') {
-		works.outsource = await client.works.findMany({
-			where: { category: 'outsource' },
-			skip,
-			take,
-		});
-	} else {
-		works = {
-			film: await client.works.findMany({
-				where: { category: 'film' },
-				skip,
-				take,
-			}),
-			short: await client.works.findMany({
-				where: { category: 'short' },
-				skip,
-				take,
-			}),
-			outsource: await client.works.findMany({
-				where: { category: 'outsource' },
-				skip,
-				take,
-			}),
-		};
-	}
-	return res.status(200).json({ success: true, works });
+  const { query } = req;
+  const { page, per_page, category, order } = query;
+  if (!page || !per_page) return;
+  const take = +per_page;
+  const skip = +per_page * (+page - 1);
+  const id = order === 'asc' ? 'asc' : 'desc';
+  let works: VideoResponseItem = {
+    film: [],
+    short: [],
+    outsource: [],
+  };
+  const restOptions: { skip: number; take: number; orderBy: { id: typeof id } } = {
+    skip,
+    take,
+    orderBy: { id: 'desc' },
+  };
+  if (category === 'film') {
+    works.film = await client.works.findMany({
+      where: { category: 'film' },
+      ...restOptions,
+    });
+  } else if (category === 'short') {
+    works.short = await client.works.findMany({
+      where: { category: 'short' },
+      ...restOptions,
+    });
+  } else if (category === 'filmShort') {
+    works.film = await client.works.findMany({
+      where: { OR: [{ category: 'film' }, { category: 'short' }] },
+      ...restOptions,
+    });
+  } else if (category === 'outsource') {
+    works.outsource = await client.works.findMany({
+      where: { category: 'outsource' },
+      ...restOptions,
+    });
+  } else {
+    works = {
+      film: await client.works.findMany({
+        where: { category: 'film' },
+        ...restOptions,
+      }),
+      short: await client.works.findMany({
+        where: { category: 'short' },
+        ...restOptions,
+      }),
+      outsource: await client.works.findMany({
+        where: { category: 'outsource' },
+        ...restOptions,
+      }),
+    };
+  }
+  return res.status(200).json({ success: true, works });
 };
 
 export default withHandler({ methods: ['GET'], handlerFunc: handler });
 
 export const config = {
-	api: {
-		externalResolver: true,
-	},
+  api: {
+    externalResolver: true,
+  },
 };
