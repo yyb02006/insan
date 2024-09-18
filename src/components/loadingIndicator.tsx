d0bd853c37b4afbdea3c486ef327767bae7428e0
alwjrqns <yyb02006@gmail.com>
2024-09-18 22:06:26 +0900

    Fix : 로그인 로딩 인디케이터의 오버레이가 가장 앞에 존재하도록 z-index 조정

--- src/components/loadingIndicator.tsx
+++ src/components/loadingIndicator.tsx
@@ -2,7 +2,7 @@ import Circles from './circles';
 
 export default function LoaidngIndicator() {
   return (
-    <div className="fixed top-0 w-screen h-screen opacity-60 z-[1] flex justify-center items-center bg-black">
+    <div className="fixed top-0 w-screen h-screen opacity-60 z-[1001] flex justify-center items-center bg-black">
       <div className="animate-spin-middle contrast-50 absolute w-[100px] aspect-square">
         <Circles
           liMotion={{
