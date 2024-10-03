fb51834420edc8c7806080adb86f7c1fab432f89
alwjrqns <yyb02006@gmail.com>
2024-10-04 00:49:53 +0900

    Feat : SearchForm 컴포넌트 요소 계층 수정 및 SSR 함수 추가

--- src/pages/work/sort.tsx
+++ src/pages/work/sort.tsx
@@ -1,8 +1,11 @@
 import Layout from '@/components/layout';
-import { MenuComponent, PostManagementLayout } from './delete';
+import { MenuComponent } from './delete';
+import PostManagementLayout from '@/components/nav/PostManagementLayout';
 import { FlatformsCategory, WorkInfos } from './write';
 import { useRef, useState } from 'react';
 import SearchForm from '@/components/searchForm';
+import { GetServerSideProps } from 'next';
+import client from '@/libs/server/client';
 
 export default function Sort() {
   const [category, setCategory] = useState<FlatformsCategory>('filmShort');
@@ -25,15 +28,45 @@ export default function Sort() {
         menuComponent: <MenuComponent />,
       }}
     >
-      <SearchForm onSearch={searchHandler} setSearchWord={setSearchWord} searchWord={searchWord} />
       <PostManagementLayout
         category={category}
         onCategoryClick={onCategoryClick}
         title="정렬하기"
         topElementRef={topElementRef}
       >
-        <div></div>
+        <SearchForm
+          onSearch={searchHandler}
+          setSearchWord={setSearchWord}
+          searchWord={searchWord}
+        />
       </PostManagementLayout>
     </Layout>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const initialWorks = {
+    filmShort: await client.works.findMany({
+      where: { OR: [{ category: 'film' }, { category: 'short' }] },
+      take: 12,
+      orderBy: { id: 'desc' },
+    }),
+    outsource: await client.works.findMany({
+      where: { category: 'outsource' },
+      take: 12,
+      orderBy: { id: 'desc' },
+    }),
+  };
+  let initialHasNextPage = { filmShort: false, outsource: false };
+  for (const count in initialWorks) {
+    initialWorks[count as FlatformsCategory].length < 12
+      ? (initialHasNextPage[count as FlatformsCategory] = false)
+      : (initialHasNextPage[count as FlatformsCategory] = true);
+  }
+  return {
+    props: {
+      initialWorks: JSON.parse(JSON.stringify(initialWorks)),
+      initialHasNextPage,
+    },
+  };
+};
