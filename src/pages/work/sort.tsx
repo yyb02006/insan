60891ed9718abe40ad3d7e09e8a2a27680d454e7
alwjrqns <yyb02006@gmail.com>
2024-10-05 23:54:04 +0900

    Feat : 필요한 상태 추가 및 searchSubmitHandler 함수 추가

--- src/pages/work/sort.tsx
+++ src/pages/work/sort.tsx
@@ -1,23 +1,54 @@
 import { GetServerSideProps } from 'next';
-import { useRef, useState } from 'react';
+import { SyntheticEvent, useRef, useState } from 'react';
 import Layout from '@/components/layout';
 import PostManagementLayout from '@/components/nav/postManagementLayout';
 import SearchForm from '@/components/searchForm';
 import client from '@/libs/server/client';
 import PostManagementMenu from '@/components/nav/postManagementMenu';
-import { FlatformsCategory, WorkInfos } from '@/pages/work/work';
+import { FlatformsCategory, VideoCollection } from '@/pages/work/work';
+import { Works } from '@prisma/client';
+import { ciIncludes } from '@/libs/client/utils';
+import Image from 'next/image';
 
-export default function Sort() {
-  const [workInfos, setWorkInfos] = useState<WorkInfos[]>([]);
-  const [category, setCategory] = useState<FlatformsCategory>('filmShort');
-  const [searchWord, setSearchWord] = useState('');
+type WorksUsedInSort = Omit<Works, 'createdAt' | 'updatedAt' | 'description'>;
+
+interface InitialData {
+  initialWorks: VideoCollection<WorksUsedInSort[]>;
+  initialHasNextPage: VideoCollection<boolean>;
+}
+
+export default function Sort({ initialWorks, initialHasNextPage }: InitialData) {
   const topElementRef = useRef<HTMLDivElement>(null);
+  const [category, setCategory] = useState<FlatformsCategory>('filmShort');
+  const [videoList, setVideoList] = useState(initialWorks);
+  const [searchWord, setSearchWord] = useState('');
+  const [searchWordSnapShot, setSearchWordSnapShot] = useState('');
+  const [searchResult, setSearchResult] =
+    useState<VideoCollection<WorksUsedInSort[]>>(initialWorks);
+  const [selectedList, setselectedList] = useState<VideoCollection<Works[]>>({
+    filmShort: [],
+    outsource: [],
+  });
+  const [hasNextPage, setHasNextPage] = useState<VideoCollection<boolean>>(initialHasNextPage);
+  const [isGrid, setIsGrid] = useState(true);
+  const [fetchLoading, setFetchLoading] = useState(false);
+  const [isSelectedListOpen, setIsSelectedListOpen] = useState(false);
+  const [page, setPage] = useState(1);
+  const perPage = 12;
   const onCategoryClick = (categoryLabel: FlatformsCategory) => {
     if (category === categoryLabel) return;
     setCategory(categoryLabel);
-    setWorkInfos([]);
   };
-  const searchHandler = () => {};
+  const searchSubmitHandler = (e: SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setPage(1); // page 부분이 2부터 시작할 필요가 있는지 살피고 수정 필요
+    setSearchWordSnapShot(searchWord);
+    const currentList = isSelectedListOpen ? selectedList[category] : videoList[category]; // 퍼킹 상상치도 못한방법
+    setSearchResult((p) => ({
+      ...p,
+      [category]: currentList.filter((li) => ciIncludes(li.title, searchWord)),
+    }));
+  };
   return (
     <Layout
       seoTitle="SORT"
@@ -35,10 +66,17 @@ export default function Sort() {
         topElementRef={topElementRef}
       >
         <SearchForm
-          onSearch={searchHandler}
+          onSearch={searchSubmitHandler}
           setSearchWord={setSearchWord}
           searchWord={searchWord}
         />
+        {searchResult[category].map((li, index) =>
+          index < 12 * page ? (
+            <div key={li.id}>
+              <Image alt={li.title} src={li.thumbnailLink} width={640} height={360} />
+            </div>
+          ) : null
+        )}
       </PostManagementLayout>
     </Layout>
   );
