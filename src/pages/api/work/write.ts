e0b01ae316589657d6fc745827106e7ae240689c
alwjrqns <yyb02006@gmail.com>
2024-10-13 14:22:07 +0900

    Feat : purpose프로퍼티가 sort인 경우에 대한 핸들러 추가

--- src/pages/api/work/write.ts
+++ src/pages/api/work/write.ts
@@ -6,13 +6,39 @@ import { NextApiRequest, NextApiResponse } from 'next';
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const {
     body: { data, secret },
+    query,
   } = req;
+  const { purpose } = query;
 
   if (secret !== process.env.NEXT_PUBLIC_ODR_SECRET_TOKEN) {
     return res.status(401).json({ success: false, message: 'Invalid token' });
   }
 
-  if (data.length > 0) {
+  if (data.length === 0) {
+    return res.status(400).json({ success: false, message: 'No data was provided' });
+  }
+
+  if (purpose === 'sort') {
+    try {
+      await client.$transaction(
+        data.map((el: { id: number; currentOrder: number; originalOrder: number }) => {
+          return client.works.update({
+            where: { id: el.id },
+            data: { order: el.currentOrder },
+          });
+        })
+      );
+      await client.revalidationStatus.upsert({
+        where: { action: 'update' },
+        create: { action: 'update', timestamp: new Date().toISOString() },
+        update: { timestamp: new Date().toISOString() },
+      });
+      return res.status(200).json({ success: true });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ success: false, error });
+    }
+  } else if (purpose === 'write') {
     try {
       await client.$transaction(
         data.map((el: WorkInfos) =>
@@ -45,18 +71,18 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         update: { timestamp: new Date().toISOString() },
       });
       /*       const revalidatePages = ['/work', '/work/write', '/work/delete'];
-      await Promise.all(
-        revalidatePages.map(async (el: string) => {
-          await res.revalidate(el);
-        })
-      ); */
+        await Promise.all(
+          revalidatePages.map(async (el: string) => {
+            await res.revalidate(el);
+          })
+        ); */
       return res.status(200).json({ success: true });
     } catch (error) {
       console.log(error);
       return res.status(500).json({ success: false, error });
     }
   } else {
-    return res.status(500).json({ success: false });
+    return res.status(400).json({ success: false, message: 'Invalid Purpose Query' });
   }
 };
 
